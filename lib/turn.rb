c9b7874988a701a18a1c9ead69a8caac7959b7f2
Joel S <joel29dec@gmail.com>
2019-03-26 16:59:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1a35d14d2d5cbbef12c7dce7b0e970b1c2a62dfe
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -10,46 +6,31 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(str)
-  num = str.to_i
-  num -= 1
-end
-
-def move (array, index, value = "X")
-  array[index] = value
-end
-
-def valid_move? (board, index)
-<<<<<<< HEAD
-  if board[index]  == "X" || board[index] == "O" || index < 0 || index > 8
-  false
-  elsif board[index] == " " || board[index] == "" || board[index] == nil && index.between?(0, 8)
-  true
+def valid_move?(board, position)
+  if position.to_i.between?(1,9)
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
   end
+
+  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
-def turn (board)
+def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
-  if index.between?(0,8)
-  valid_move?(board, index)
-  move(board, index)
-  display_board(board)
+  if valid_move?(board, input)
+    move(board, input)
   else
-  turn(board)
+    turn(board)
   end
-end
-=======
-  if board[index] == " " || board[index] == "" || board[index] == nil && index.between?(0, 8)
-  true
-  elsif board[index]  == "X" || board[index] == "O" || index <= -1 || index > 8
-  false
-  end
+  display_board(board)
 end
 
-board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-puts valid_move?(board1, 0)
-puts valid_move?(board1, 4)
-puts valid_move?(board1, -1)    
->>>>>>> 1a35d14d2d5cbbef12c7dce7b0e970b1c2a62dfe
+def position_taken?(board, location)
+  board[location] != " "
+end
+
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
+end
